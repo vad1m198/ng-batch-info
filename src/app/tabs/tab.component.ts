96ff285868a708327cfc84ce123e5568a39acd91
vad1m198 <vadim.merkotan@silverlinecrm.com>
2017-06-27 07:21:25 +0300

    tabs chages

--- src/app/tabs/tab.component.ts
+++ src/app/tabs/tab.component.ts
@@ -1,19 +1,35 @@
-import { Component, Input, TemplateRef, Directive,Optional } from "@angular/core";
+import { Component, Input, TemplateRef, Directive,Optional,ContentChild,QueryList } from "@angular/core";
+
+@Directive({selector: '[sl-tab-heading]'})
+export class SlTabHeading {
+  constructor(public templateRef: TemplateRef<any>) {}
+}
+
+@Directive({selector: '[sl-tab-content]'})
+export class SlTabContent {
+  constructor(public templateRef: TemplateRef<any>) {}
+}
 
 @Directive({
-  selector: 'ng-template[sl-tab]',
-  exportAs: 'slTab',
+  selector: 'sl-tab'
 })
 export class SlTab {
   @Input() Id: string | number;
-  @Input() heading: string | TemplateRef<any>;
+  @Input() heading: TemplateRef<any>;
   private _active: boolean = false;
+  @ContentChild(SlTabContent) contentTemplate: SlTabContent;
+  @ContentChild(SlTabHeading) headingTemplate: SlTabHeading;
 
   constructor(@Optional() public templateRef: TemplateRef<any>) {}
 
-    ngAfterContentInit() {
-      console.log('SlTab ngAfterContentInit', this.heading)
+  ngAfterContentInit() {
+    console.log('this.headingTemplate',this.headingTemplate);
+    console.log('this.contentTemplate',this.contentTemplate);
+    if (this.headingTemplate) {
+      this.heading = this.headingTemplate.templateRef;
     }
+    this.templateRef = this.contentTemplate.templateRef;
+  }
 
   set active(active: boolean) {
     this._active = active;
@@ -22,14 +38,4 @@ export class SlTab {
   get active(): boolean {
     return this._active;
   }
-}
-
-@Directive({selector: 'ng-template[sl-tab-heading]'})
-export class SlTabHeading {
-  constructor(public templateRef: TemplateRef<any>) {}
-}
-
-@Directive({selector: 'ng-template[sl-tab-content]'})
-export class SlTabContent {
-  constructor(public templateRef: TemplateRef<any>) {}
 }
